e0eb05118c5230b4f0b13e94b55894d5180fcf2a
CarolBerna <54364606+CarolBerna@users.noreply.github.com>
2024-06-21 10:28:30 +0300

    Update ap.js

--- ap.js
+++ ap.js
@@ -1,19 +1,25 @@
+document.addEventListener('DOMContentLoaded', function (){
+const menubtn = document.getElementById('mobile);
 const bar = document.getElementById('bar');
 const close = document.getElementById('close');
 const nav = document.getElementById('navbar');
 
+
+menubtn.addEventListener('click', function (){
+  
+   nav.classList.toggle('active');
+   menubtn.classList.toggle('active');
+
 // Add event listener for opening menu
-if (bar) {
-    bar.addEventListener('click', () => {
-        nav.classList.add('active');
+if (menubtn.classList.contains('active')) {
+    bar.classList.remove('fa-bars');
+    bar.classList.add('fa-times');
     });
+} 
+else{ 
+  
+   bar.classList.remove('fa-times');
+   bar.classList.add('fa-bars');
 }
-
-// Add event listener for closing menu
-if (close) {
-    close.addEventListener('click', () => {
-        nav.classList.remove('active');
-    });
-}
-
+});
 
