4409c23ae1e27360aafe9fa8fe4f5adf5e7c8590
Carol Bernadette <carolbernadette35@gmail.com>
2024-06-21 16:15:27 +0300

    responsive

--- ap.js
+++ ap.js
@@ -1,19 +1,27 @@
-const bar = document.getElementById('bar');
-const close = document.getElementById('close');
-const nav = document.getElementById('navbar');
-
-// Add event listener for opening menu
-if (bar) {
-    bar.addEventListener('click', () => {
-        nav.classList.add('active');
+document.addEventListener('DOMContentLoaded', function (){
+    const menubtn = document.getElementById('mobile');
+    const bar = document.getElementById('bar');
+    const close = document.getElementById('close');
+    const nav = document.getElementById('navbar');
+    
+    
+    menubtn.addEventListener('click', function (){
+        nav.classList.toggle('Active');
+        menubtn.classList.toggle('Active');
+     
+     // Add event listener for opening menu
+     if (menubtn.classList.contains('Active')) {
+         bar.classList.remove('fa-bars');
+         bar.classList.add('fa-times');
+         }
+        else{ 
+        bar.classList.remove('fa-times');
+        bar.classList.add('fa-bars');
+     }
     });
-}
 
-// Add event listener for closing menu
-if (close) {
-    close.addEventListener('click', () => {
-        nav.classList.remove('active');
-    });
-}
 
+});
+    
+    
 
