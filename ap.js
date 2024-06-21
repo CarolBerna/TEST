93ab261127cfa9943896452d901cd66164fb955c
Carol Bernadette <carolbernadette35@gmail.com>
2024-06-21 16:35:21 +0300

    hopeful

--- ap.js
+++ ap.js
@@ -1,53 +1,24 @@
-document.addEventListener('DOMContentLoaded', function (){
-<<<<<<< HEAD
+document.addEventListener('DOMContentLoaded', function() {
     const menubtn = document.getElementById('mobile');
     const bar = document.getElementById('bar');
     const close = document.getElementById('close');
     const nav = document.getElementById('navbar');
-    
-    
-    menubtn.addEventListener('click', function (){
-        nav.classList.toggle('Active');
-        menubtn.classList.toggle('Active');
-     
-     // Add event listener for opening menu
-     if (menubtn.classList.contains('Active')) {
-         bar.classList.remove('fa-bars');
-         bar.classList.add('fa-times');
-         }
-        else{ 
-        bar.classList.remove('fa-times');
-        bar.classList.add('fa-bars');
-     }
+
+    menubtn.addEventListener('click', function() {
+        nav.classList.toggle('active');
+        menubtn.classList.toggle('active');
+
+        // Toggle menu icon
+        if (menubtn.classList.contains('active')) {
+            bar.classList.remove('fa-bars');
+            bar.classList.add('fa-times');
+        } else {
+            bar.classList.remove('fa-times');
+            bar.classList.add('fa-bars');
+        }
     });
-
-=======
-const menubtn = document.getElementById('mobile);
-const bar = document.getElementById('bar');
-const close = document.getElementById('close');
-const nav = document.getElementById('navbar');
-
-
-menubtn.addEventListener('click', function (){
-  
-   nav.classList.toggle('active');
-   menubtn.classList.toggle('active');
-
-// Add event listener for opening menu
-if (menubtn.classList.contains('active')) {
-    bar.classList.remove('fa-bars');
-    bar.classList.add('fa-times');
-    });
-} 
-else{ 
-  
-   bar.classList.remove('fa-times');
-   bar.classList.add('fa-bars');
-}
 });
->>>>>>> eaba014561c3d1abe314647bae0f1c29decba33b
 
-});
     
     
 
